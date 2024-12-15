9107f31a6dfe086dc3bc7df81ee7d14c39a0c4c5
Dominik <dmk.tomaszewski@gmail.com>
2024-12-15 17:43:23 +0100

    first view

--- src/mobile_app/ekomonitor/lib/main.dart
+++ src/mobile_app/ekomonitor/lib/main.dart
@@ -1,7 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+final List<SettingsButtonConfig> settingsList = [
+  const SettingsButtonConfig(
+    text: "Ustawienia pogody",
+    color: Colors.blueGrey,
+    icon: Icon(Icons.settings),
+    path: '/weather-settings',
+  ),
+  const SettingsButtonConfig(
+    text: "Ustawienia aplikacji",
+    color: Colors.grey,
+    icon: Icon(Icons.settings),
+    path: '/app-settings',
+  ),
+  const SettingsButtonConfig(
+    text: "Ustawienia uzytkownika",
+    color: Colors.deepPurple,
+    icon: Icon(Icons.settings),
+    path: '/user-settings',
+  ),
+];
+
+class WeatherSettingsView extends StatelessWidget {
+  const WeatherSettingsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ustawienia pogody'),
+      ),
+      body: ListView(
+        children: [
+          const ListTile(
+            title: Text("Tytuł"),
+            subtitle: Text("Podtytuł"),
+            leading: Icon(Icons.ac_unit),
+          ),
+          const ListTile(
+            title: Text("Tytuł"),
+            subtitle: Text("Podtytuł"),
+            leading: Icon(Icons.ac_unit),
+          ),
+          const ListTile(
+            title: Text("Tytuł"),
+            subtitle: Text("Podtytuł"),
+            leading: Icon(Icons.ac_unit),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MainTile extends StatelessWidget {
+  final String subtitle;
+  final String title;
+  final Color color;
+  final IconData icon;
+
+  const MainTile({
+    super.key,
+    required this.color,
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8),
+      child: SizedBox(
+          width: double.infinity,
+          height: 130,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                color: color,
+                width: double.infinity,
+                height: double.infinity,
+              ),
+              Positioned(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(subtitle),
+                      Text(title,
+                          style: Theme.of(context).textTheme.headlineMedium),
+                    ],
+                  ),
+                  left: 8,
+                  bottom: 8),
+              const Positioned(
+                  child: Icon(
+                    Icons.abc,
+                    size: 64,
+                  ),
+                  right: 8,
+                  top: 8),
+            ],
+          )),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -13,113 +119,158 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/weather-settings': (context) => const WeatherSettingsView(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatelessWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Witaj Developerze'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("Witaj w aplikacji Ekomonitor"),
+              const MainTile(
+                color: Colors.red,
+                title: 'Tytuł',
+                subtitle: 'Podtytuł',
+                icon: Icons.ac_unit,
+              ),
+              const Divider(),
+              const Text("Warunki pogodowe"),
+              const WeatherCarousel(),
+              const Divider(),
+              const Text("Warto zwrócić na to uwagę"),
+              const ListTile(
+                title: Text("Tytuł"),
+                subtitle: Text("Podtytuł"),
+                leading: Icon(Icons.ac_unit),
+              ),
+              const ListTile(
+                title: Text("Tytuł"),
+                subtitle: Text("Podtytuł"),
+                leading: Icon(Icons.ac_unit),
+              ),
+              const ListTile(
+                title: Text("Tytuł"),
+                subtitle: Text("Podtytuł"),
+                leading: Icon(Icons.ac_unit),
+              ),
+              const Divider(),
+              const Text("Ustawienia"),
+              Row(
+                children: settingsList
+                    .map((config) =>
+                        Expanded(child: SettingsButton(config: config)))
+                    .toList(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class WeatherCarousel extends StatelessWidget {
+  const WeatherCarousel({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return const SizedBox(
+      height: 130,
+      child: CarouselView(
+          scrollDirection: Axis.horizontal,
+          itemExtent: double.infinity,
+          children: [
+            SizedBox(
+              width: 100,
+              height: 100,
+              child: Placeholder(),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(
+              width: 100,
+              height: 100,
+              child: Placeholder(),
+            ),
+            SizedBox(
+              width: 100,
+              height: 100,
+              child: Placeholder(),
+            ),
+          ]),
+    );
+  }
+}
+
+// SETTINGS BUTTON
+
+class SettingsButtonConfig {
+  final String text;
+  final Color color;
+  final Icon icon;
+  final String path;
+
+  const SettingsButtonConfig(
+      {this.text = "Ustawienia",
+      this.color = Colors.grey,
+      this.icon = const Icon(Icons.settings),
+      this.path = "/"});
+}
+
+class SettingsButton extends StatelessWidget {
+  final SettingsButtonConfig config;
+
+  const SettingsButton({super.key, required this.config});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Navigator.pushNamed(context, config.path),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(color: config.color),
+        ),
+        height: 100,
+        width: double.infinity,
+        child: Stack(
+          children: [
+            Positioned(
+              child: config.icon,
+              right: 8,
+              top: 8,
+            ),
+            Positioned(
+              child: Text(
+                config.text,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                softWrap: true,
+              ),
+              left: 8,
+              bottom: 8,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
