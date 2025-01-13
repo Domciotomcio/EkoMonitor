2b5e4602091031e41e4cc352d275558e093999ee
MrRKD <radoslaw.dubieniecki@gmail.com>
2025-01-13 21:25:28 +0100

    Change User Profiling API port

--- src/user_profiling/api_up.py
+++ src/user_profiling/api_up.py
@@ -84,4 +84,4 @@ def api_delete_user_answer(answer_id: int):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8002)
+    uvicorn.run(app, host="0.0.0.0", port=8005)
